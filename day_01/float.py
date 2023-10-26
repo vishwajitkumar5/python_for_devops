232ee7fdc6d15339a192b8de170f76f9157bb1dd
vishwajit kumar <vishawjitkumar@gmail.com>
2023-10-26 13:26:41 +0530

    day01

---
+++ day_01/float.py
@@ -0,0 +1,27 @@
+num1 = 10.5
+num2 = 2.5
+
+#for addition
+result1 = num1 + num2
+print("addition: ",result1)
+
+#for substration
+
+result2 = num2 - num1
+print("substration of two number is :",result2)
+
+#for multiplaction
+result3 = num1 * num2
+print("multiplication of two number is :",result3)
+
+#for division
+result4 = num1 / num2
+print("division of two number is :",result4)
+
+#for reminder
+result5 = num1 % num2
+print("reminder of two number is :",result5 )
+
+#for Absolute Value
+result6 = abs(-7.5)
+print("Absolute Value:", result6)
