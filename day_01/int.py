041d96360b6dc8c63258f89151999dbb5b435407
vishwajit kumar <vishawjitkumar@gmail.com>
2023-10-26 13:23:50 +0530

    day01

---
+++ day_01/int.py
@@ -0,0 +1,28 @@
+num1 = 5
+num2 = 10
+
+#for addition
+
+result1 = num1 + num2
+print("addition of two number is :", result1)
+
+#for substration
+
+result2 = num2 - num1
+print("substration of two number is :",result2)
+
+#for multiplaction
+result3 = num1 * num2
+print("multiplication of two number is :",result3)
+
+#for division
+result4 = num1 / num2
+print("division of two number is :",result4)
+
+#for reminder
+result5 = num1 % num2
+print("reminder of two number is :",result5 )
+
+#for Absolute Value
+result6 = abs(-7)
+print("Absolute Value:", result6)
